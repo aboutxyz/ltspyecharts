7873e17cdb22c5ae519fa3f8c49e5d9f2aad9b2d
aboutxyz <haishangmuxucao@dlmu.edu.cn>
2017-09-17 12:15:25 +0800

    12:15

--- lts-data-preview/__init__.py
+++ lts-data-preview/__init__.py
@@ -4,10 +4,17 @@ import requests,json
 from requests import get
 from flask import Flask, render_template,request,session
 import MySQLdb
+from config import *
 
 app = Flask(__name__)
 app.secret_key = 'forget it you can never guess'
 error_msg = u'请输入船名航次'
+
+
+# def surround_by_quote(vai):
+    # return '"%s"' % vai
+# env=app.jinja_env
+# env.filters["surround_by_quotes"] = surround_by_quote   #前面为html 中用的，后面是函数名
 
 @app.route('/', methods=["GET","POST"])
 def index():
@@ -15,8 +22,8 @@ def index():
 	
 @app.route('/statics/<vname>', methods=["GET","POST"])
 def revoyage(vname):
-    if vname=="m-v":
-        return render_template("statics/m-v.html")
+    if vname=="month-voyage":
+        return render_template("statics/month-voyage.html",voyagevaluedict=voyagevaluedict)
 
 if __name__ == '__main__':
     app.run(debug=True)
