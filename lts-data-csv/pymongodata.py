498a315cd8e02d8a8036e435e374c97127241c9c
aboutxyz <haishangmuxucao@dlmu.edu.cn>
2017-09-18 23:01:40 +0800

    23:01

--- lts-data-csv/pymongodata.py
+++ lts-data-csv/pymongodata.py
@@ -1,3 +1,4 @@
+%%time
 #! python2
 #coding:utf-8
 
@@ -11,13 +12,21 @@ collection = db.data
 
 voyagename = ['CAT','NEAX','PAN NKT1','SITC NJ1','SITC NA1','SCT', 'NT1','ST2','CPS','CPF','CHS','CTI','EASNSP']
 
+
+
 pipeline = [{"$group": {"_id" :{"voyage":"$航线"} , "count": {"$sum": "$TEU"}}}]
 
-datadict= {}
-for j in list(collection.aggregate(pipeline)):
-    datadict[j["_id"]["voyage"]] = j["count"]
-
-datalist = []
-for i in voyagename:
-    datalist.append(datadict[i])
+datalist = [0 for i in range(len(voyagename))]
+for i in list(collection.aggregate(pipeline)):
+    if i["_id"]["voyage"] in voyagename:
+        datalist[voyagename.index(i["_id"]["voyage"])] = i["count"]
+        
 print datalist
+
+# datadict= {}
+# for j in list(collection.aggregate(pipeline)):
+    # datadict[j["_id"]["voyage"]] = j["count"]
+# print sorted(datadict.iteritems(),key = lambda x:voyagename.index(x[0]))
+
+
+# 试试json
